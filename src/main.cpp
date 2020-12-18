e3df7477dc6e8c5b0f1b88e730a3d015af980c50
CAHEK <sstukalovs2005@gmail.com>
2020-12-18 14:26:41 +0200

    It's glad, Carl! Hope it'll be right

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,48 @@
-#include <iostream>
-using namespace std;
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main(int argc, char* argv[])
+#include <iostream>
+int main(void)
 {
-	cout<<"Dogiezz"<<endl;
-	return 0;
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+	if(!gladLoadGL())
+	{
+		std::cout<<"Can't load 'glad'"<<std::endl;
+		return -1;
+	}
+	std::cout<<"OpenGL "<<GLVersion.major <<"."<<GLVersion.minor<<std::endl;
+	
+	glClearColor(0,0,1,1);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
